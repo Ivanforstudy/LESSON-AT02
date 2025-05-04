d2466e71c6a648be819616f15098698bf4fb674f
IvanTusanin <ivantusanin@gmail.com>
2025-05-05 00:51:54 +0300

    ADD

---
+++ test.py
@@ -0,0 +1,19 @@
+import unittest
+import pytest
+from main import init_db, add_user, get_user
+
+@pytest.fixture
+def db_conn():
+   conn = init_db()
+   yield conn
+   conn.close()
+
+def test_add_or_get_user(db_conn):
+    add_user(db_conn, "Sasha", 30)
+    user = get_user(db_conn, "Sasha")
+    assert user == (1, "Sasha", 30)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
